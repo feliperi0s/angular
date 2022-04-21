7a71237ba28f03572106c31efa6ca645b097129d
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-04-20 21:58:35 -0300

    aula3 angular

--- exercicios/src/app/app-routing.module.ts
+++ exercicios/src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
+import { MenuComponent } from './menu/menu.component';
+import { NetflixComponent } from './netflix/netflix.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [
   {
     path : '',
-    redirectTo: 'login',
+    redirectTo: 'netflix',
     pathMatch: 'full',
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+  },{
+    path : 'netflix',
+  component: NetflixComponent,
+  },
+  {
+    path : 'menu',
+    component: MenuComponent,
   }
+  
 ];
 
 @NgModule({
--- exercicios/src/app/app.module.ts
+++ exercicios/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { NetflixComponent } from './netflix/netflix.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NetflixComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
