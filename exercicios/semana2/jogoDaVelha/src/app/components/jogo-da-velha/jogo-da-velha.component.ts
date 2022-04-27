506f11bfc7839aec7cd135137ccd5f8cead648b4
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-04-26 23:47:51 -0300

    jogo da velha

--- exercicios/semana2/jogoDaVelha/src/app/components/jogo-da-velha/jogo-da-velha.component.ts
+++ exercicios/semana2/jogoDaVelha/src/app/components/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ReturnStatement } from '@angular/compiler';
 
 @Component({
   selector: 'app-jogo-da-velha',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  public a1:string=''
+  public a2:string=''
+  public a3:string=''
+  public b1:string=''
+  public b2:string=''
+  public b3:string=''
+  public c1:string=''
+  public c2:string=''
+  public c3:string=''
+
+  public primeiro:string='';
+  public aletori:number=Math.floor(Math.random() * 10);
+  public xisOuZero:string = ''
+  public venc:string = ''
+
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+    this.quemComeca()
   }
+
+  public quemComeca(){
+    if (this.aletori%2 == 0) {
+      this.primeiro='X'
+    }else{
+      this.primeiro='O'
+    }
+  }
+
+
+  public mudarJogador(){
+    if (this.primeiro == 'X') {
+      this.primeiro = 'O'
+    }else{
+      this.primeiro = 'X'
+    }
+  }
+
+  public vencedor() {
+    if (this.a1 && this.a2 && this.a3 == "X" || this.b1 && this.b2 && this.b3 == "X" || this.c1 && this.c2 && this.c3 == "X" || this.a1 && this.b1 && this.c1 == "X" || this.a3 && this.b3 && this.c3 == "X" || this.a1 && this.b1 && this.c1 == "X" || this.a1 && this.b2 && this.c3 == "X" || this.a3 && this.b2 && this.c1 == "X") {
+     this.venc = "O vencedor é X"
+    }if(this.a1 && this.a2 && this.a3 == "O" || this.b1 && this.b2 && this.b3 == "O" || this.c1 && this.c2 && this.c3 == "O" || this.a1 && this.b1 && this.c1 == "O" || this.a3 && this.b3 && this.c3 == "O" || this.a1 && this.b1 && this.c1 == "O" || this.a1 && this.b2 && this.c3 == "O" || this.a3 && this.b2 && this.c1 == "O") {
+      this.venc = "O vencedor é O"
+    }
+  }
+
+ public zerar() {
+  if (this.a1 = 'X' || "O" , this.a2 = 'X' || "O") {
+    this.a1 = ''
+  }
+ }
 
 }
